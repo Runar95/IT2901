c6a195c1aa55283ca42fdaed1791fb87e38aad18
svein007 <svein_001@hotmail.com>
2019-01-30 13:02:49 +0100

    #12 - Create door logic

---
+++ Assets/Resources/Scripts/Globals.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Globals {
+    public static bool[] openDoors = new bool[4];
+
+    static Globals() {
+        openDoors[0] = true;
+        openDoors[1] = false;
+
+        Debug.Log("OpenDoors: " + openDoors[0] + ", " + openDoors[1] + ", " + openDoors[2] + "," + openDoors[3]);
+    }
+
+    public static void resetOpenDoors() {
+        openDoors = new bool[4];
+    }
+
+    public static void setDoor(int door, bool open) {
+        openDoors[door - 1] = open;
+    }
+
+    public static bool getDoor(int door) {
+        return openDoors[door -1];
+    }
+}
