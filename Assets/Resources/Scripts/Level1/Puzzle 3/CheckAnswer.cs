85dc1c5b6e2de6e159575deb0a9744366ee8e3ca
andreski <andreski@stud.ntnu.no>
2019-01-31 13:08:25 +0100

    #2 - Pulled functionality out of blocks and into a script

---
+++ Assets/Resources/Scripts/Level1/Puzzle 3/CheckAnswer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class Check_Answer : MonoBehaviour
+{
+  public Flowchart flowchart;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
