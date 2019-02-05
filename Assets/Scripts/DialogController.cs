5c26a55013ec3c1174c3d707bfc2a648b3fa8ca2
Sigurd <snv97@hotmail.com>
2019-02-05 22:59:40 +0100

    #8 almost implemented the NPC dialog system completly

---
+++ Assets/Scripts/DialogController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+
+/*
+This class is meant to assure that one cannot start dialogs whilst another one is in progress. 
+     
+*/
+public class DialogController : MonoBehaviour
+{
+    //bool that says if someone is performing dialog
+    private static bool performingDialog = false;
+    
+
+    //used when some character wants to perform dialog
+    public static bool ClaimDialog(Flowchart flowchart)
+    {
+        if (flowchart.HasExecutingBlocks() || performingDialog == true)
+        {
+            return false;
+        }
+        performingDialog = true;
+        return true;
+    }
+
+    //used when some character is releasing the dialog for other people to use it
+    public static void ReleaseDialog()
+    {
+        performingDialog = false;
+    }
+}
---
+++ Assets/Scripts/SpearManController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpearManController : NpcController
+{
+    //sets currentBlockNr on start
+    void Start()
+    {
+        base.currentBlockNr = base.nrOfBlocks;
+    }
+
+    //overrides the method that sets what block should be set next
+    //this particular implementation plays them in the opposite order of default
+    protected override void UpdateOnClickBlock()
+    {
+        if(base.currentBlockNr >= 1)
+        {
+            base.onClickBlock = base.character.name + base.currentBlockNr;
+            base.currentBlockNr--;
+            return;
+        }
+        base.currentBlockNr = base.nrOfBlocks;
+        UpdateOnClickBlock();
+    }
+
+}
