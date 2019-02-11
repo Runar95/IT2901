d12008d6412841cc348f2302b3fc9856a5eee48d
Andreas <andreski@stud.ntnu.no>
2019-02-11 23:00:06 +0100

    Hotfix - Start blocks once mode set to onSceneStart, also added autoselect to codepads

--- Assets/CodeValidation.cs
+++ Assets/CodeValidation.cs
@@ -11,21 +11,22 @@ public class CodeValidation : MonoBehaviour
 	//CONSTANTS
 	public const string CODE1_SOLUTION = "ar9y761m";
 	public const string CODE2_SOLUTION = "pu44m3k9";
-	
-	
+
 	public Flowchart flowchart;
 	public GameObject Codepanel_Input;
 	public InputField code_input;
-	
+
 	public static bool code1_solved = false;
 	public static bool code2_solved = false;
-	
+
 	void Start() {
 		var se= new InputField.SubmitEvent();
 		se.AddListener(SubmitName);
 		code_input.onEndEdit = se;
+		code_input.ActivateInputField();
+		code_input.Select();
 	}
-	
+
 	private void SubmitName(string arg0)
     {
 		EventSystem.current.SetSelectedGameObject(null);
@@ -33,29 +34,28 @@ public class CodeValidation : MonoBehaviour
 		if (!flowchart.GetBooleanVariable("L_R")) {
 			if (arg0.Equals(CODE1_SOLUTION)) {
 				code1_solved = true;
-				flowchart.ExecuteBlock("Feedback_Codepanel_Correct");				
+				flowchart.ExecuteBlock("Feedback_Codepanel_Correct");
 			} else {
-				flowchart.ExecuteBlock("Feedback_Codepanel_Incorrect");				
+				flowchart.ExecuteBlock("Feedback_Codepanel_Incorrect");
 			}
 		} else {
 			if (arg0.Equals(CODE2_SOLUTION)) {
 				code2_solved = true;
-				flowchart.ExecuteBlock("Feedback_Codepanel_Correct");				
+				flowchart.ExecuteBlock("Feedback_Codepanel_Correct");
 			} else {
 				flowchart.ExecuteBlock("Feedback_Codepanel_Incorrect");
 			}
-			
 		}
-        checkAnswer();
+      checkAnswer();
     }
-	
+
 	public void checkAnswer() {
 		if (code1_solved && code2_solved) {
 			Globals.setControlroomDoor(false);
 			updateDoor();
 		}
 	}
-	
+
 	public void updateDoor() {
 		flowchart.SetBooleanVariable("CtrlRoomLocked", Globals.getControlroomDoor(1));
 		flowchart.ExecuteBlock("Update_Controlroom_Door");
