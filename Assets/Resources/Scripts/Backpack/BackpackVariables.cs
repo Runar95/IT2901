c2fb7c24313f1b7bb52697ee2b2a5444610564d9
supertosse <supertosse@hotmail.com>
2019-02-14 09:42:56 +0100

    Fix stuff

---
+++ Assets/Resources/Scripts/Backpack/BackpackVariables.cs
@@ -0,0 +1,8 @@
+﻿
+
+public static class BackpackVariables
+{
+    public enum Item { Empty, Coin, Stone, Man, Mushroom, Face, CoffeeCup ,Star };
+    public static Item[] itemsInBackpack = { Item.Empty, Item.Empty, Item.Empty, Item.Empty, Item.Empty, Item.Empty, Item.Empty, Item.Empty, Item.Empty, Item.Empty, Item.Empty };
+
+}
---
+++ Assets/Resources/Scripts/Backpack/ToggleBackpack.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleBackpack : MonoBehaviour
+{
+    public GameObject canvas;
+ 
+    public void OnMouseDown()
+    {
+        Debug.Log("wewe");
+        canvas.SetActive(!canvas.activeSelf);
+    }
+
+
+}
