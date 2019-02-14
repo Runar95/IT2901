1cd342dae2821ccffa78c8d2ca011c4a0f652b7a
supertosse <supertosse@hotmail.com>
2019-02-14 15:23:31 +0100

    add scalable map

---
+++ Assets/Resources/Scripts/Backpack/Map.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Map : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject map;
+    public bool showMap;
+    Animator anim;
+    public void Start()
+    {
+        map.SetActive(showMap);
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        anim.SetBool("Big", true);
+        //transform.localScale += new Vector3(0.7F, 0.7F, 0);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        anim.SetBool("Big", false);
+        // transform.localScale += new Vector3(-0.7F, -0.7F, 0);
+    } 
+}
