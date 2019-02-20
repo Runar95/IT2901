b02d9780879f5fa298f22f65460a818d8783022d
supertosse <supertosse@hotmail.com>
2019-02-20 15:09:39 +0100

    Add positions on map

--- Assets/Resources/Scripts/Backpack/Map.cs
+++ Assets/Resources/Scripts/Backpack/Map.cs
@@ -6,23 +6,69 @@ using UnityEngine.EventSystems;
 public class Map : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public GameObject map;
-    public bool showMap;
     Animator anim;
+    public GameObject YouAreHere;
+    Animator positionAnim;
+    public enum mapPosition{MH_View1, MH_View2, MH_View3, MH_View4, CH_View1, CH_View2, CH_View3, CH_View4, EH_View1, EH_View2, EH_View3, EH_View4}
+ 
     public void Start()
     {
-        map.SetActive(showMap);
         anim = gameObject.GetComponent<Animator>();
+        positionAnim = YouAreHere.GetComponent<Animator>();
     }
 
-    public void OnPointerEnter(PointerEventData eventData)
+    public void OnPointerEnter(PointerEventData eventData)//When mouse is over map, make it bigger
     {
         anim.SetBool("Big", true);
-        //transform.localScale += new Vector3(0.7F, 0.7F, 0);
     }
 
-    public void OnPointerExit(PointerEventData eventData)
+    public void OnPointerExit(PointerEventData eventData)//When mouse is no longer over map, make it smaler 
     {
         anim.SetBool("Big", false);
-        // transform.localScale += new Vector3(-0.7F, -0.7F, 0);
     } 
+
+    public void updatePosition(mapPosition position){
+      switch (position)
+      {
+          case mapPosition.MH_View1:
+                positionAnim.SetTrigger("MH_View1");
+              break;
+          case  mapPosition.MH_View2:
+                positionAnim.SetTrigger("MH_View2");
+              break;
+          case  mapPosition.MH_View3:
+                positionAnim.SetTrigger("MH_View3");
+              break;
+          case  mapPosition.MH_View4:
+                positionAnim.SetTrigger("MH_View4");
+              break;
+          case mapPosition.EH_View1:
+                positionAnim.SetTrigger("EH_View1");
+              break;
+          case  mapPosition.EH_View2:
+                positionAnim.SetTrigger("EH_View2");
+              break;
+          case  mapPosition.EH_View3:
+                positionAnim.SetTrigger("EH_View3");
+              break;
+          case  mapPosition.EH_View4:
+                positionAnim.SetTrigger("EH_View4");
+              break;
+          case mapPosition.CH_View1:
+                positionAnim.SetTrigger("CH_View1");
+              break;
+          case  mapPosition.CH_View2:
+                positionAnim.SetTrigger("CH_View2");
+              break;
+          case  mapPosition.CH_View3:
+                positionAnim.SetTrigger("CH_View3");
+              break;
+          case  mapPosition.CH_View4:
+                positionAnim.SetTrigger("CH_View4");
+              break;
+         default:
+              break;
+      }
+
+    }
 }
