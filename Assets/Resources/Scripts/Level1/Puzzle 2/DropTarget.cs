71d072be57eba0ed67177aabbc93e9ec10c44245
Johannes Weiberg-Aurdal <johannes@dhcp-10-22-13-11.wlan.ntnu.no>
2019-02-20 10:48:01 +0100

    Added snap-to-place function and hover effect

---
+++ Assets/Resources/Scripts/Level1/Puzzle 2/DropTarget.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropTarget : MonoBehaviour
+{
+
+    private GameObject lastCollider;
+    public GameObject correctValue;
+    public GameObject cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        lastCollider = collision.gameObject;
+        if (lastCollider != null) {
+
+            // Snap crayon to target
+            Vector3 pos = gameObject.transform.position;
+            pos.z = -1;
+            lastCollider.SendMessage("SetSnapPos", pos);
+            lastCollider.SendMessage("SetLastTarget", gameObject.transform);
+
+            // Show rect
+            gameObject.GetComponent<SpriteRenderer>().enabled = true;
+
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D c)
+    {
+        lastCollider = c.gameObject;
+        Vector3 pos = gameObject.transform.position;
+        pos.z = -1;
+        lastCollider.SendMessage("SetSnapPos", pos);
+        lastCollider.SendMessage("SetLastTarget", gameObject.transform);
+        if (c.gameObject == correctValue)
+        {
+            cam.GetComponent<L1_P2Logic>().cc.Add("" + correctValue.GetInstanceID());
+        }
+        cam.GetComponent<L1_P2Logic>().pc.Add("" + correctValue.GetInstanceID());
+        cam.GetComponent<L1_P2Logic>().CheckLevelComplete();
+    }
+
+    void OnTriggerExit2D(Collider2D collision) {
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        collision.gameObject.SendMessage("SetSnapPos", Vector3.zero);
+        collision.gameObject.SendMessage("ResetLastTarget");
+
+        lastCollider = null;
+
+        if (collision.gameObject == correctValue)
+        {
+            cam.GetComponent<L1_P2Logic>().cc.Remove("" + correctValue.GetInstanceID());
+        }
+        cam.GetComponent<L1_P2Logic>().pc.Remove("" + correctValue.GetInstanceID());
+    }
+}
