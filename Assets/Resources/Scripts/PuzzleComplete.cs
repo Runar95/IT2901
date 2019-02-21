ded73fafc8acc7cc31bd92fec1b0ada8c72f91f4
andreski <andreski@stud.ntnu.no>
2019-02-21 14:35:45 +0100

    hotfix/#2 - fixed P4L1

--- Assets/Resources/Scripts/PuzzleComplete.cs
+++ Assets/Resources/Scripts/PuzzleComplete.cs
@@ -7,17 +7,6 @@ public class PuzzleComplete : MonoBehaviour {
     public int level = 0;
     public int puzzle = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     // Open next puzzle
     public void FinishLevel() {
