83b620e6e1f51b0ef7b914043dc2b30e1f6a66cc
supertosse <supertosse@hotmail.com>
2019-03-14 11:23:23 +0100

    issue99 add introscene inside ship

---
+++ Assets/Resources/Assets/Scripts/Backpack/CallFungusBlock.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class CallFungusBlock : MonoBehaviour {
+
+    Flowchart flowchart;
+
+
+    public string blockName;
+
+    void Start() {
+        flowchart = GameObject.Find("Flowchart").GetComponent<Flowchart>();
+
+    }
+
+    void OnMouseDown() {
+        flowchart.ExecuteBlock(blockName);
+    }
+    
+}
+
