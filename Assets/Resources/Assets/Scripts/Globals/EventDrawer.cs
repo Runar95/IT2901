43e3a9a9801e80924703f938ec4a4944750c8c76
sigurdnv <snv97@hotmail.com>
2019-03-20 09:23:04 +0100

    fixed event drawer, so that it creates a queue of messages to draw from

--- Assets/Resources/Assets/Scripts/Globals/EventDrawer.cs
+++ Assets/Resources/Assets/Scripts/Globals/EventDrawer.cs
@@ -1,34 +1,59 @@
-﻿using System.Collections;
+﻿﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EventDrawer : MonoBehaviour
 {
     //boolean value indicating whether or not a message should be drawn on screen at any given time
-    private static bool displayMessage, initiateMessage = false; 
+    private static bool displayMessage, initiateMessage = false;
     //message to be printed on screen due to event having occured
-    private static string message;
+    private static List<string> messages = new List<string>();
     //style to use when making the message appear on screen
     private GUIStyle gs = new GUIStyle();
 
+    //initializes variables before scenes are loaded
     private void Awake()
     {
         gs.fontSize = 30;
         gs.normal.textColor = Color.black;
     }
 
+    //action to be done upon loadin a scene where the EwentDrawer is used
+    private void Start()
+    {
+          //if a message did not play to completion last time a scene was loaded,
+          //it is now discarded
+          if(displayMessage)
+          {
+              displayMessage = false;
+              messages.RemoveAt(0);
+              if(messages.Count != 0)
+              {
+                  initiateMessage = true;
+              }
+          }
+    }
+
     public static void DrawMessage(string text)
     {
-        message = text;
-        initiateMessage = true;
+        messages.Add(text);
+        if(messages.Count == 1)
+        {
+            initiateMessage = true;
+        }
     }
 
 
     IEnumerator ShowMessage()
     {
         displayMessage = true;
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(3);
         displayMessage = false;
+        messages.RemoveAt(0);
+        if(messages.Count != 0)
+        {
+            initiateMessage = true;
+        }
     }
 
     private void Update()
@@ -43,7 +68,7 @@ public class EventDrawer : MonoBehaviour
     private void OnGUI()
     {
         if (displayMessage) {
-            GUI.Label(new Rect( (Screen.width / 8), (Screen.height / 8), 200, 200), message, gs);
+            GUI.Label(new Rect( (Screen.width / 8), (Screen.height / 8), 200, 200), messages[0], gs);
         }
     }
 }
