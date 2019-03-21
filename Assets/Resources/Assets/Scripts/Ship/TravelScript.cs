70f16aff500d894b74f32b28a21e45f0e21399af
svein007 <svein_001@hotmail.com>
2019-03-21 15:56:40 +0100

    #109 Add bg change on next level

--- Assets/Resources/Assets/Scripts/Ship/TravelScript.cs
+++ Assets/Resources/Assets/Scripts/Ship/TravelScript.cs
@@ -15,17 +15,34 @@ public class TravelScript : MonoBehaviour
     public Sprite lvl2Window;
     public Sprite lvl3Window;
 
+    public SpriteChanger mainHall1;
+    public SpriteChanger mainHall2;
+    public SpriteChanger mainHall3;
+    public SpriteChanger mainHall4;
+
     void Start()
     {
         if (Globals.level == 1) {
             //Debug.Log("Level 1");
             controlroomWindow.sprite = lvl1Window;
+            mainHall1.ChangeSpriteTo(0);
+            mainHall2.ChangeSpriteTo(0);
+            mainHall3.ChangeSpriteTo(0);
+            mainHall4.ChangeSpriteTo(0);
         } else if (Globals.level == 2) {
             //Debug.Log("Level 2");
             controlroomWindow.sprite = lvl2Window;
+            mainHall1.ChangeSpriteTo(1);
+            mainHall2.ChangeSpriteTo(1);
+            mainHall3.ChangeSpriteTo(1);
+            mainHall4.ChangeSpriteTo(1);
         } else if (Globals.level == 3) {
             //Debug.Log("Level 3");
             controlroomWindow.sprite = lvl3Window;
+            mainHall1.ChangeSpriteTo(2);
+            mainHall2.ChangeSpriteTo(2);
+            mainHall3.ChangeSpriteTo(2);
+            mainHall4.ChangeSpriteTo(2);
         }
 
         screenNewDest.SetActive(Globals.nextLevelAvailable);
