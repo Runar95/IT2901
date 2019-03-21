f102e5aab847d9134a98fe5de625cfbe6b48c880
Johannes Weiberg-Aurdal <johannes@dhcp-10-22-78-226.wlan.ntnu.no>
2019-03-21 11:26:30 +0100

    #104 Updated L1_P1

---
+++ Assets/Resources/Assets/Scripts/Levels/Level 1/Puzzle 1/L1_P1Logic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class L1_P1Logic : MonoBehaviour
+{
+
+    public HashSet<string> cc;
+    public HashSet<string> pc;
+    public Flowchart flowchart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = new HashSet<string>();
+        pc = new HashSet<string>();
+    }
+
+    public void CheckLevelComplete()
+    {
+        if (cc.Count == 6)
+        {
+            // TODO: call fungus block, narrative dialog, return to level
+            flowchart.ExecuteBlock("PuzzleFinish");
+        }
+        else if (pc.Count == 6)
+        {
+            flowchart.ExecuteBlock("PuzzleIncorrect");
+        }
+    }
+}
