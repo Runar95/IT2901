f94472a0d961db59acc3e56ebe8bbcff3b692dc4
supertosse <supertosse@hotmail.com>
2019-03-21 15:56:16 +0100

    #133 update puzzle

---
+++ Assets/Resources/Assets/Scripts/Levels/Level 3/Puzzle 2/Logic.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class Logic : MonoBehaviour
+{
+
+    public HashSet<int> cc; // Correct crayon set
+    public HashSet<int> pc; // Placed crayon set
+    public Flowchart flowchart;
+
+    public bool userIsNotified = false;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        cc = new HashSet<int>();
+        pc = new HashSet<int>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+    public void CheckLevelComplete() {
+        if (!userIsNotified) {
+            if (IsLevelComplete()) {
+                flowchart.ExecuteBlock("PuzzleFinish");
+                userIsNotified = true;
+            } else if (pc.Count == 6) {
+                flowchart.ExecuteBlock("PuzzleIncorrect");
+                userIsNotified = true;
+            }
+        }
+    }
+
+    public bool IsLevelComplete() {
+        return cc.Count == 6;
+    }
+
+    public void AddPlacedCrayon(int hash) {
+        pc.Add(hash);
+    }
+
+    public void AddCorrectCrayon(int hash) {
+        cc.Add(hash);
+    }
+
+    public void RemovePlacedCrayon(int hash) {
+        pc.Remove(hash);
+    }
+
+    public void RemoveCorrectCrayon(int hash) {
+        cc.Remove(hash);
+    }
+
+}
---
+++ Assets/Resources/Assets/Scripts/Levels/Level 3/Puzzle 2/Target.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{   
+
+    public GameObject correctCrayon;
+    public GameObject cam;
+
+    private GameObject lastCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Cryon enters target-collider
+    void OnTriggerEnter2D(Collider2D collision) {
+        lastCollider = collision.gameObject;
+        if (lastCollider != null) {
+            Vector3 pos = gameObject.transform.position;
+            pos.z = -1;
+            lastCollider.SendMessage("SetSnapPos", pos);
+            lastCollider.SendMessage("SetLastTarget", gameObject.transform);
+
+            // Show rect
+            gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        }
+
+        Logic logic = cam.GetComponent<Logic>();
+
+        if (collision.gameObject == correctCrayon) {
+            logic.AddCorrectCrayon(correctCrayon.GetInstanceID());
+        }
+        logic.AddPlacedCrayon(collision.gameObject.GetInstanceID());
+
+    }
+
+    void OnTriggerStay2D(Collider2D c) {
+        lastCollider = c.gameObject;
+        Vector3 pos = gameObject.transform.position;
+        pos.z = -1;
+        lastCollider.SendMessage("SetSnapPos", pos);
+        lastCollider.SendMessage("SetLastTarget", gameObject.transform);
+    }
+
+    // Cryon exits target-collider
+    void OnTriggerExit2D(Collider2D collision) {
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        collision.gameObject.SendMessage("SetSnapPos", Vector3.zero);
+        collision.gameObject.SendMessage("ResetLastTarget");
+
+        Logic logic = cam.GetComponent<Logic>();
+
+        lastCollider = null;
+
+        if (collision.gameObject == correctCrayon) {
+            cam.GetComponent<Logic>().cc.Remove(correctCrayon.GetInstanceID());
+            logic.RemoveCorrectCrayon(correctCrayon.GetInstanceID());
+        }
+
+        logic.RemovePlacedCrayon(collision.gameObject.GetInstanceID());
+        logic.userIsNotified = false;
+    }
+}
