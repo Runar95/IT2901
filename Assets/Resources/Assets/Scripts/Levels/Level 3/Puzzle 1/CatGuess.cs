bcd4df35015be108a7595758ef7f4da5c2860e6a
Stian Tøsse <supertosse@hotmail.com>
2019-03-29 12:34:39 +0100

    #145 add puzzle l3p1

---
+++ Assets/Resources/Assets/Scripts/Levels/Level 3/Puzzle 1/CatGuess.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using Fungus;
+using UnityEngine.SceneManagement;
+
+public class CatGuess : MonoBehaviour, IPointerClickHandler {
+    public InputField inputField;
+    public string correctNumber;
+    private Flowchart flowchart;
+    public int incorrectGuesses = 0;
+
+    void Start() {
+        flowchart = GameObject.Find("Flowchart").GetComponent<Flowchart>();
+    }
+
+    //Detect if a click occurs
+    public void OnPointerClick(PointerEventData pointerEventData){
+        //Use this to tell when the user left-clicks on the Button
+        if (pointerEventData.button == PointerEventData.InputButton.Left){
+            if (inputField.text.Trim().ToLower() == correctNumber) {
+                flowchart.ExecuteBlock("CorrectGuess");
+            } else {
+                // Show user they answered incorrectly
+                flowchart.ExecuteBlock("IncorrectGuess");
+                // Incr. incorrect counter
+                incorrectGuesses++;
+                // Show help if >3
+                if (incorrectGuesses > 3) {
+                    // Show BIG help (fungus block)
+                } else {
+                    // Show SMALL help (?) (fungus block)
+                }
+
+            }
+
+        }
+    }
+}
