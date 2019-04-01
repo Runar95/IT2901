816ab2762e749728a1e9defaa2faf76ceee3c537
svein007 <svein_001@hotmail.com>
2019-04-01 19:59:31 +0200

    Fix L1P1 Fungus build

---
+++ Assets/Resources/Assets/Scripts/Navigation/BackButton.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class BackButton : MonoBehaviour
+{
+
+    public Sprite hoverSprite;
+
+    private Sprite normalSprite;
+    private SpriteRenderer spriteRenderer;
+    private Flowchart flowchart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
+        this.flowchart = GameObject.Find("Flowchart").GetComponent<Flowchart>();
+        this.normalSprite = spriteRenderer.sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // OnEnter, OnExit => hover effect
+    // OnClick => LoadScene or MoveCamera (fungus block???)
+
+    void OnMouseEnter() 
+    {
+        spriteRenderer.sprite = hoverSprite;
+    }
+
+    void OnMouseExit() {
+        spriteRenderer.sprite = normalSprite;
+    }
+
+    void OnMouseUpAsButton() {
+        flowchart.ExecuteBlock("MainViewBlock");
+    }
+
+
+}
