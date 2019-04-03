6680753f39cee36a0bb8ab7237c4bcf8789b2ef9
svein007 <svein_001@hotmail.com>
2019-04-03 11:13:45 +0200

    Travel back button fix

---
+++ Assets/Resources/Assets/Scripts/UnloadUnused.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnloadUnused: MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Unload());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Unload() {
+        yield return new WaitForSeconds(3);
+        Resources.UnloadUnusedAssets();
+    }
+}
