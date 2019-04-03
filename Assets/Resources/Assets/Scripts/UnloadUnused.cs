ce4c6ab706f13b73d2a179b6071dade477de56da
svein007 <svein_001@hotmail.com>
2019-04-03 13:13:05 +0200

    Fix Build Resolution, Framerate, Intro

---
+++ Assets/Resources/Assets/Scripts/UnloadUnused.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnloadUnused: MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(1280, 720, false, 60);
+        Application.targetFrameRate = 60;
+        StartCoroutine(Unload());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Unload() {
+        yield return new WaitForSeconds(3);
+        Resources.UnloadUnusedAssets();
+    }
+}
