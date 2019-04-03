c98462c3a8959ae8a5bfa024176d211e1a76313d
sigurdnv <snv97@hotmail.com>
2019-04-03 14:39:30 +0200

    #152 Updated the timer endstate with progression

--- Assets/Resources/Assets/Scripts/Story/EndStatePlayerTimeOut.cs
+++ Assets/Resources/Assets/Scripts/Story/EndStatePlayerTimeOut.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using Fungus;
 
 public class EndStatePlayerTimeOut : MonoBehaviour
 {
     //keeps track of which part of a scene to play
     private int act = 0;
+
+    //flowchart with dialog
+    Flowchart flowchart;
 
     //sprites to be changed in the scene
     public Sprite blackScreen;
@@ -15,11 +19,16 @@ public class EndStatePlayerTimeOut : MonoBehaviour
     public Sprite countdown2;
     public Sprite countdown3;
     public Sprite timeIsOut;
+    public Sprite vandreren;
+    public Sprite wanderersInfrontOfScreen;
+    public Sprite knowledgeCollage;
+    public Sprite foodCollage;
+    public Sprite friendsCollage;
+
 
     private bool playNewScene = false;
     private bool showTimesOut = false;
 
-  
     private SpriteRenderer screen;
     private Vector3 screenPos;
 
@@ -32,6 +41,8 @@ public class EndStatePlayerTimeOut : MonoBehaviour
     {
         //retrieves needed variables from the scene
         GameObject screenObj = GameObject.Find("controlroom_screen");
+        GameObject flowchartObject = GameObject.Find("Flowchart");
+        flowchart = flowchartObject.GetComponent<Flowchart>();
         screenPos = screenObj.transform.localPosition;
         screen = screenObj.GetComponent<SpriteRenderer>();
         playNewScene = true;
@@ -50,11 +61,14 @@ public class EndStatePlayerTimeOut : MonoBehaviour
                 StartCoroutine("TimesOut");
                 break;
             case 3:
+                StartCoroutine("VillainMonolog");
+                break;
+            case 4:
                 StartCoroutine("ShowProgressOnScreen");
                 break;
         }
     }
-    
+
     //counts down on screen in scene
     IEnumerator ScreenCountDown()
     {
@@ -83,8 +97,11 @@ public class EndStatePlayerTimeOut : MonoBehaviour
     {
         showTimesOut = true;
         StartCoroutine("FlashTimesOut");
-        yield return new WaitForSeconds(5f);
-        showTimesOut = false;
+        yield return new WaitForSeconds(1f);
+        flowchart.ExecuteBlock("dialog");
+        while(flowchart.HasExecutingBlocks()){
+            yield return new WaitForSeconds(0.3f);
+        }
         playNewScene = true;
     }
 
@@ -99,12 +116,60 @@ public class EndStatePlayerTimeOut : MonoBehaviour
         }
     }
 
+    //plays the part of the scene where the villain is performing a monolog
+    IEnumerator VillainMonolog()
+    {
+        showTimesOut = false;
+        screen.sprite = blackScreen;
+        SpriteRenderer wanderer = GameObject.Find("ship_outside").GetComponent<SpriteRenderer>();
+        for(int i = 1; i < 6; i++)
+        {
+            if(i == 1)
+            {
+                for(int j = 0; j <= 200; j++)
+                {
+                    yield return new WaitForSeconds(0.01f);
+                    screen.color = new Color(1f, 1f, 1f, 1f - 0.005f*j);
+                }
+                flowchart.ExecuteBlock("villainMonolog");
+            }else
+            {
+                flowchart.ExecuteBlock("villainMonolog_part" + i);
+            }
+            while (flowchart.HasExecutingBlocks())
+            {
+                yield return new WaitForSeconds(0.5f);
+            }
+
+            switch(i)
+            {
+                case 1:
+                    screen.color = new Color(1f, 1f, 1f, 1f);
+                    screen.sprite = wanderersInfrontOfScreen;
+                    break;
+                case 2:
+                    screen.color = new Color(1f, 1f, 1f, 0f);
+                    break;
+                case 3:
+                    screen.color = new Color(1f, 1f, 1f, 1f);
+                    screen.sprite = knowledgeCollage;
+                    break;
+                case 5:
+                    screen.color = new Color(1f, 1f, 1f, 0f);
+                    break;
+            }
+        }
+        screen.sprite = blackScreen;
+        screen.color = new Color(1f, 1f, 1f, 1f);
+        playNewScene = true;
+    }
+
     IEnumerator ShowProgressOnScreen()
     {
         global::Globals.Progress prog = Globals.GetProgress();
         int l = prog.level;
         int p = prog.puzzle;
-        progressMessage = "Du kom deg til puzzle " + p + " på nivå " + l + ".";
+        progressMessage = "Dere nådde puzzle " + p + " på nivå " + l + ".";
         drawProgress = true;
         yield return null;
     }
